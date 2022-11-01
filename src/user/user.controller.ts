72da19f4a9e87d63e7a61bcc587bbdaf6855654c
慕哲学长 <1904250095@qq.com>
2022-11-01 15:43:14 +0800

    change

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  Body,
-  Put,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
 @Controller('user')
